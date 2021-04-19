8efec96b7616e25f361c109d00936ef695896dd9
Pjfranco14 <69051176+Pjfranco14@users.noreply.github.com>
2021-04-19 17:33:05 -0600

    Add files via upload

---
+++ js/Main.js
@@ -0,0 +1,8 @@
+function Main(){
+    
+    var et_nombre = document.getElementeById("ipt_nombre").value;
+    var et_edad = document.getElementById("ipt_edad").value;
+
+    console.log(et_nombre)
+    console.log(et_edad)
+}
