a6ad197b5db833324cada9b56ad549b3fe5cdc1b
Pjfranco14 <69051176+Pjfranco14@users.noreply.github.com>
2021-04-26 10:47:49 -0600

    Add files via upload
    calculadora funcional

--- js/Main.js
+++ js/Main.js
@@ -1,8 +1,46 @@
-function Main(){
-    
-    var et_nombre = document.getElementeById("ipt_nombre").value;
-    var et_edad = document.getElementById("ipt_edad").value;
+//-------------------------------------------------------------------------------//
+function sumar(){
+    var num1 = document.getElementById("num-1").value
+    var num2 = document.getElementById("num-2").value
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+    var caja_respuesta = document.getElementById("caja_respuesta")
+    var respuesta = num1+num2
+    caja_respuesta.textContent = respuesta
 
-    console.log(et_nombre)
-    console.log(et_edad)
 }
+
+    var btn_s = document.getElementById("btn_s")
+    btn_s.addEventListener('click', sumar)
+//-------------------------------------------------------------------------------//
+function restar(){
+    var num1 = document.getElementById("num-1").value
+    var num2 = document.getElementById("num-2").value
+    var caja_respuesta = document.getElementById("caja_respuesta")
+    var respuesta = num1-num2
+    caja_respuesta.textContent = respuesta
+}
+    var btn_r = document.getElementById("btn_r")
+    btn_r.addEventListener('click', restar)
+//-------------------------------------------------------------------------------//
+function multiplicar(){
+    var num1 = document.getElementById("num-1").value
+    var num2 = document.getElementById("num-2").value
+    var caja_respuesta = document.getElementById("caja_respuesta")
+    var respuesta = num1*num2
+    caja_respuesta.textContent = respuesta
+}
+var btn_m = document.getElementById("btn-m")
+btn_m.addEventListener('click', multiplicar)
+//-------------------------------------------------------------------------------//
+function dividir(){
+    var num1 = document.getElementById("num-1").value
+    var num2 = document.getElementById("num-2").value
+    var caja_respuesta = document.getElementById("caja_respuesta")
+    var respuesta = num1/num2
+    caja_respuesta.textContent = respuesta
+}
+var btn_d = document.getElementById("btn-d")
+btn_d.addEventListener('click', dividir)
+//------------------------------------------------------------------------//
+
